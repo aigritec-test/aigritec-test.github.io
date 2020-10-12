e69e2804dc99faec37719ef2ee4f9f9d9a1f337c
Oscar <oscarligthart@gmail.com>
2020-10-12 14:42:03 +0200

    Updates

---
+++ precache-manifest.164b99193504dcb5408e3f922d081256.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c891d2bb278a6f7358b1dd4f9dc2bd3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b35052dd63756df6c925",
+    "url": "/static/css/main.3ff71ec6.chunk.css"
+  },
+  {
+    "revision": "19df340f73e74a84293a",
+    "url": "/static/js/2.2d7ec3cd.chunk.js"
+  },
+  {
+    "revision": "d9404789d94346d951655b0b19bb0e47",
+    "url": "/static/js/2.2d7ec3cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b35052dd63756df6c925",
+    "url": "/static/js/main.16e83dc0.chunk.js"
+  },
+  {
+    "revision": "ff975beb67d742df777c",
+    "url": "/static/js/runtime-main.b18067ab.js"
+  }
+]);
